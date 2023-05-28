3753b5034f3b0a1e07fdcce446511c91dbac02d7
michelle nderi <nderimichelle@gmail.com>
2023-05-28 16:08:10 +0300

    Completed author methods

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,21 @@ require_relative '../lib/author'
 require_relative  '../lib/magazine'
 require_relative '../lib/article'
 
+author = Author.new("John Doe")
+magazine1 = Magazine.new("Fashion Weekly", "Fashion")
+magazine2 = Magazine.new("Tech Today", "Technology")
+
+author.add_article(magazine1, "Latest Fashion Trends")
+author.add_article(magazine2, "The Future of Technology")
+author.add_article(magazine1, "Fashion Tips")
+
+puts "Author: #{author.name}"
+puts "Author's Articles:"
+author.articles.each { |article| puts "- #{article.title}" }
+
+puts "\nAuthor's Magazines:"
+author.magazines.each { |magazine| puts "- #{magazine.name}" }
+
+puts "\nAuthor's Topic Areas:"
+author.topic_areas.each { |topic| puts "- #{topic}" }
 
--- lib/author.rb
+++ lib/author.rb
@@ -7,9 +7,17 @@ class Author
      def articles
         Article.all.select {|article|article.author == self}
      end
+    def magazines
+        articles.map {|article|article.magazine}.uniq
+    end
 
-     
+    def add_article(magazine, title)
+        Article.new(self, magazine, title)
+      end
+    
+    def topic_areas
+        magazines.map { |magazine| magazine.category }.uniq
+    end   
  end 
 
- author = Author.new("John Doe")
-puts author.name 
+
